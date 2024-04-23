bc4852fb0aaacfa52cfcbb3c0b40a1bc007abe7d
Aleix <aleixpochvila@gmail.com>
2024-04-23 20:38:48 +0200

    html funcionant

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
-from flask import Flask, request, url_for
+from flask import Flask, request, url_for, render_template
 import register
 
 app = Flask(__name__)
+@app.route("/")
+def startapp():
+    return render_template('register.html')
+
+
 
 @app.route("/api/register/", methods=['POST'])
 def api():
-    JSONRebut = request.json
-    correu = JSONRebut['email']
+    #JSONRebut = request.json
+    correu = request.form['email']
     resultat = register.gravarRegisterDB(correu)
     return resultat+'\n'
+
 
 
     
